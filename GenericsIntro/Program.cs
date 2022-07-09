a5a362a9a80b75d46cdad47e7280e58a6b81ad3b
Reena-dll <ouz.spy@gmail.com>
2022-07-09 14:43:59 +0300

    9 Haziran Son Ders

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsimler List");
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Oğuzhan");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Ahmet");
+            Console.WriteLine(isimler.Length);
+
+            foreach (string item in isimler.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.WriteLine("Sayilar List");
+            MyList<int> sayilar = new MyList<int>();
+            sayilar.Add(500);
+            Console.WriteLine(sayilar.Length);
+            sayilar.Add(600);
+            Console.WriteLine(sayilar.Length);
+            sayilar.Add(700);
+            Console.WriteLine(sayilar.Length);
+
+            foreach (int item in sayilar.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
