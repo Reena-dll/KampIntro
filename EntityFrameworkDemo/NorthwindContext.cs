83e5b27fceb3b45c7ee74a6eb14a5ad417c4387d
Reena-dll <ouz.spy@gmail.com>
2022-07-16 13:02:59 +0300

    16 Temmuz Gün Sonu

---
+++ EntityFrameworkDemo/NorthwindContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkDemo
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=True");
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+
+
+}
---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetAll();
+            GetProductsByCategory(1);
+            Console.ReadLine();
+        }
+
+        private static void GetAll()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                foreach (var item in context.Products)
+                {
+                    Console.WriteLine(item.ProductName);
+                }
+            }
+        }
+
+        private static void GetProductsByCategory(int cateogryId)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = context.Products.Where(p => p.CategoryId == cateogryId);
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item.ProductName);
+                }
+            }
+        }
+
+    }
+
+}
